3ffaa644b666fbdcc0c5d7a6b35c79902625d919
JB-Tellez <jb.tellez@gmail.com>
2022-10-31 23:49:03 -0700

    adds data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
 
 export default function Home() {
+
+
+  const [resources, setResources] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = {
+        username: 'jb',
+        password: 'jb',
+      }
+
+      const baseUrl = 'https://lightsail-snacks.9hg33nqeuilhu.us-west-2.cs.amazonlightsail.com';
+
+      const tokenResponse = await axios.post(baseUrl + '/api/token/', data);
+
+      const tokens = tokenResponse.data;
+
+      const resourceResponse = await axios.get(baseUrl + '/api/v1/snacks/', config(tokens));
+
+      setResources(resourceResponse.data);
+
+
+    }
+    fetchData();
+  }, []);
+
+  function config(tokens) {
+
+    return {
+      headers: {
+        'Authorization': 'Bearer ' + tokens.access
+      }
+    };
+  }
+
   return (
     <div>
-      Hi there
+      {JSON.stringify(resources)}
     </div>
   )
 }
